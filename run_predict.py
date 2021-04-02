683201a6ac5db5e3cc08017afb4cf7a991d768a6
Jackson <635725626@qq.com>
2021-04-02 16:49:24 +0800

    add predict

--- run_predict.py
+++ run_predict.py
@@ -1,6 +1,9 @@
 from transformers import BertTokenizer
 from model.BertClassificationModel import BertForSequenceClassification
 import torch
+import sys
+
+
 checkpoint_dir = 'bert-base-chinese'
 
 
@@ -13,12 +16,14 @@ def predict(model, tokenizer, text):
     token_type_ids = torch.tensor([tokenized_dict['token_type_ids']]).long()
     attention_mask = torch.tensor([tokenized_dict['attention_mask']]).long()
     _, logits = model(input_ids=input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)
-    pred_label = torch.argmax(logits, 1)
+    pred_label = torch.argmax(logits, 1).data
     return logits, pred_label
     #pred_label = torch.argmax(logits)
 
 
 if __name__ == '__main__':
-    text = '我今天真的好开心啊'
+    labels = {'0':'喜悦', '1':'愤怒', '2':'厌恶', '3':'低落'}
+    text = sys.argv[1]
     logits, pred_label = predict(model, tokenizer, text)
-    print(logits, pred_label)
+    print('四种感情的打分分别为：', str(logits.data))
+    print('预测的情感结果为：',labels[str(int(pred_label.data))] )
